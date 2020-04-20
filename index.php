6814580615792fb7f96b564290d0d58797e5ffd8
Dom-Gabriel <ghabrielaugusto@gmail.com>
2020-04-19 22:22:31 -0300

    Primeiros arquivos

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tutto L'amore Site de Relacionamento Sério</title>
+	</head>
+
+	<body>
+		<p>ESSE SITE ESTÁ EM CONSTRUÇÃO</p>
+		<br><br><br>
+		<p>Por Gabriel A. L. da Conceição</p>
+	</body>
+</html>
